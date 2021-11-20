0ef08873b35e35c14d06f26c135b10ddec838fe4
zarrar ahmed <ahmedzarrar64@gmail.com>
2021-11-20 23:26:30 +0500

    thired commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var camera, scene, renderer, controls,vartices;
+var camera, scene, renderer, controls,vartices, line,j, vartices2;
 var sphere, cube;
+
 init();
+j=vartices2.length-1;
 animate();
 
 function init() {
@@ -19,29 +21,59 @@ function init() {
   var plane = new THREE.GridHelper(100, 10);
   scene.add(plane);
   
+  //vartices 1
+  
   var vartices =[];
 	var r =3;
-  var R=12;
-  for(var o=0;o<= 2*Math.PI;o+=0.1){
+  for(var j=0;j<= 2*Math.PI;j+=0.2){
   for(var i=0; i<= 2*Math.PI;i+=0.01){
-  var x = (R+(r*Math.cos(o)))*Math.cos(i);
-  var y = (R+(r*Math.cos(o)))*Math.sin(i);
-  var z = r* Math.sin(o);
+  var x = r* Math.cos(i)*Math.sin(j);
+  var y = r* Math.sin(i)*Math.sin(j);
+  var z= r* Math.cos(j);
   
   vartices.push(new THREE.Vector3(x,y,z));
-  }
-  }
+  } }
+  
   geometry = new THREE.BufferGeometry();
   geometry.setFromPoints(vartices);
   
   var material= new THREE.LineBasicMaterial({color:0x0000ff});
-  var line = new THREE.Line(geometry,material);
+   line = new THREE.Line(geometry,material);
+  /* line.rotation.x-=1.6 */;
   scene.add(line);
+  
+  
+  
+  // vartices 2
+  
+ vartices2 = [];
+  var r =15;
+  for(var i=0; i<= 7*Math.PI;i+=0.01){
+  var a = r* Math.cos(i);
+  var c = r* Math.sin(i);
+  var b = r*i*0.2;
+  
+  
+  vartices2.push(new THREE.Vector3(a,b,c));
+  }
+  geometry = new THREE.BufferGeometry();
+  geometry.setFromPoints(vartices2);
+  
+  var material= new THREE.LineBasicMaterial({color:0x00ff00});
+   var helix = new THREE.Line(geometry,material);
+  /* helix.rotation.x-=1.6 */;
+  scene.add(helix);
+ 
+
 
 }
 var delta;
 function animate() {
+/* line.rotation.y+=0.1 */
   requestAnimationFrame(animate);
+  line.position.set(vartices2[j].x,vartices2[j].y,vartices2[j].z);
+  j-=5;
+  if(j< 0) j=vartices2.length-1;
   render();
 }
 
